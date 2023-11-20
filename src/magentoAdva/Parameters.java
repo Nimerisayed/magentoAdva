53ff0affc3abdb363411645987560e5aa35caf0f
Nimerisayed <nimirisayed@gmail.com>
2023-11-20 16:13:18 +0300

    update

---
+++ src/magentoAdva/Parameters.java
@@ -0,0 +1,10 @@
+package magentoAdva;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Parameters {
+	static String MyURL = "https://magento.softwaretestingboard.com/";
+
+	static WebDriver driver = new ChromeDriver();
+}
